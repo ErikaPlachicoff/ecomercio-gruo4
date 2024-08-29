55600a182d556953119e1052c7647d5b7a2ce234
selemist <168042467+selemist@users.noreply.github.com>
2024-08-29 15:03:33 -0300

    Cambios en Products
    Tras el cambio de diseño de products.html, se realizó sus respectivos cambios en products css y js.
    Esto fue realizado por Agustina Delgado y Selene Echartea.
    
    Co-Authored-By: suggus7 <174636039+suggus7@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -1,35 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const spinner = document.getElementById('spinner-wrapper');
-  const productList = document.getElementById('product-list'); 
-  const PRODUCTS_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.json'; 
+  const productList = document.getElementById('product-list');
+  const PRODUCTS_URL = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
 
   // Función para hacer una solicitud al API y obtener un archivo JSON con la información de los productos (autos).
   getJSONData(PRODUCTS_URL).then(function (result) {
-    if (result.status === "ok") {      
+    if (result.status === "ok") {
       console.log(result);
 
       // Recorre cada producto y crea el HTML correspondiente
       result.data.products.forEach(product => {
-        const productElement = document.createElement('div'); 
-        productElement.classList.add('col-12', 'col-md-6', 'col-lg-4'); 
+        const productElement = document.createElement('div');
+        productElement.classList.add('col-12', 'col-md-6', 'col-lg-4');
         productElement.innerHTML = `
-          <div class="card mb-3">
-            <img src="${product.image}" class="card-img-top" alt="${product.name}">
-            <div class="card-body">
-              <h5 class="card-title"><strong>${product.name}</strong></h5>
-              <p class="card-text">${product.description}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Precio:</strong> ${product.currency} ${product.cost}</li>
-              <li class="list-group-item"><strong>Vendidos:</strong> ${product.soldCount}</li>
-            </ul>
-          </div>
-        `; 
-        productList.appendChild(productElement); 
+  <div class="card mb-3">
+    <div class="image-container position-relative">
+      <img src="${product.image}" class="card-img-top" alt="${product.name}">
+      <div class="overlay position-absolute bottom-0 start-0 end-0 text-white d-flex flex-column justify-content-center align-items-center">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item bg-transparent border-0 "><strong>Precio:</strong> ${product.currency} ${product.cost}</li>
+          <li class="list-group-item bg-transparent border-0 "><strong>Vendidos:</strong> ${product.soldCount}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="card-body text-center">
+      <h5 class="card-title"><strong>${product.name}</strong></h5>
+      <p class="card-text">${product.description}</p>
+    </div>
+  </div>
+`;
+        productList.appendChild(productElement);
       });
     }
   })
-  .catch(error => {
-    console.error('Error al obtener productos:', error); 
-  });
+    .catch(error => {
+      console.error('Error al obtener productos:', error);
+    });
 });
